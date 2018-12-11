117ad7a072d26b2672388d53485b368ed0242270
Vladislav <37692577+Frolovvlad@users.noreply.github.com>
2018-12-11 14:01:31 +0600

    Add files via upload

---
+++ eli2018/js/common.js
@@ -0,0 +1,36 @@
+/* ----------------------------------------------------------------
+            [ Вызов ( Scrollit ) ]
+----------------------------------------------------------------*/
+
+$.scrollIt({
+	upKey: 38, // key code to navigate to the next section
+	downKey: 40, // key code to navigate to the previous section
+	easing: 'swing', // the easing function for animation
+	scrollTime: 600, // how long (in ms) the animation takes
+	activeClass: 'active', // class given to the active nav element
+	onPageChange: null, // function(pageIndex) that is called when page is changed
+	topOffset: -150 // offste (in px) for fixed top navigation
+});
+
+
+/* ----------------------------------------------------------------
+                [ Закрытие Меню ]
+-----------------------------------------------------------------*/
+
+
+const nav = document.querySelector('nav'),
+	links = nav.querySelectorAll('.nav-item'),
+	toggler = nav.querySelector('.navbar-toggler'),
+	collapse = nav.querySelector('.navbar-collapse');
+
+for (let link of links) {
+	link.addEventListener('click', () => {
+		toggler.classList.toggle('collapsed');
+		toggler.dataset.ariaExpanded = false;
+		collapse.classList.toggle('show');
+	})
+}
+
+/* ----------------------------------------------------------------
+                [ --- ]
+-----------------------------------------------------------------*/
